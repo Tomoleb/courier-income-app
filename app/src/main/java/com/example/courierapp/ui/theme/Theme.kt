89ec155c6a3e9aebed4a1661c64d44e88efce71d
Tomoleb <149542007+Tomoleb@users.noreply.github.com>
2026-05-04 19:53:15 +0300

    Create Theme.kt
    обязателен для темы приложения

---
+++ app/src/main/java/com/example/courierapp/ui/theme/Theme.kt
@@ -0,0 +1,32 @@
+package com.example.courierapp.ui.theme
+
+import android.app.Activity
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+private val LightColorScheme = lightColorScheme()
+private val DarkColorScheme = darkColorScheme()
+
+@Composable
+fun CourierAppTheme(
+    darkTheme: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
+    }
+    MaterialTheme(
+        colorScheme = colorScheme,
+        content = content
+    )
+}
