5152f6e681fd9b53ab7fbc1934b2172948f6c20d
Tomoleb <149542007+Tomoleb@users.noreply.github.com>
2026-05-04 23:17:46 +0300

    Add TypeConverters annotation to AppDatabase
    Подключен TypeConverter к Room

--- app/src/main/java/com/example/courierapp/AppDatabase.kt
+++ app/src/main/java/com/example/courierapp/AppDatabase.kt
@@ -2,8 +2,10 @@ package com.example.courierapp
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(entities = [IncomeRecord::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun incomeDao(): IncomeDao
 }
